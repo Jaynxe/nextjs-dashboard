e6a28f2dab22c98761d6289e908e7a9b3781a4c3
xjy <2742665196@qq.com>
2025-05-08 12:32:50 +0800

    finish App Router

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,34 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+    title: 'Invoice_Edit',
+};
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+    const params = await props.params;
+    const id = params.id;
+    const [invoice, customers] = await Promise.all([
+        fetchInvoiceById(id), //通过id获取发票信息。
+        fetchCustomers(), //获取下拉菜单中的客户名称。
+    ]);
+    if (!invoice) {
+        notFound();
+    }
+    return (
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Invoices', href: '/dashboard/invoices' },
+                    {
+                        label: 'Edit Invoice',
+                        href: `/dashboard/invoices/${id}/edit`,
+                        active: true,
+                    },
+                ]}
+            />
+            <Form invoice={invoice} customers={customers} />
+        </main>
+    );
+}
